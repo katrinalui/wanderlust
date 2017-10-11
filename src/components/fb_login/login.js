0c29acdd3a9263c74164490c2177b1d15201c8e6
Katrina Lui <katrinawlui@gmail.com>
2017-10-11 11:04:01 -0700

    Prevent FB login button from rerendering

--- src/components/fb_login/login.js
+++ src/components/fb_login/login.js
@@ -12,12 +12,35 @@ import {
 import * as firebase from 'firebase';
 
 class FacebookLogin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false
+    };
+  }
+
+  buttonStyle() {
+    if (this.state.isLoggedIn) {
+      return {
+        height: 0,
+        opacity: 0
+      };
+    } else {
+      return {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      };
+    }
+  }
+
   render() {
+    const style = this.buttonStyle();
+
     return (
-      <View>
+      <View style={style}>
         <LoginButton
           readPermissions={["public_profile", "email"]}
-          autoLogoutLink={false}
           onLoginFinished={
             (error, result) => {
               if (error) {
@@ -25,6 +48,7 @@ class FacebookLogin extends React.Component {
               } else if (result.isCancelled) {
                 alert("Login cancelled.");
               } else {
+                this.setState({ isLoggedIn: true });
                 AccessToken.getCurrentAccessToken().then(
                   (data) => {
 
