523d73201f38b3e061467821b3610366ecedcda1
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-10-12 10:21:49 -0700

    Implement dashboard index

--- src/reducers/selectors.js
+++ src/reducers/selectors.js
@@ -1,7 +1,7 @@
 export const selectUserTrips = state => {
   const currentUser = state.ui.session.currentUser;
   if (currentUser && currentUser.trips) {
-    return Object.values(currentUser.trips);
+    return Object.keys(currentUser.trips).map(id => { return {[id]: currentUser.trips[id] };});
   } else {
     return [];
   }
